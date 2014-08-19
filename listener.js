ca74d37c57df38e549e51ad71ad5379ce3ef30a4
Mike Shi <michaelshi42@gmail.com>
2014-08-18 17:55:09 -0700

    Implemented Registrar Website Url Finding and added registrar dictionary

--- listener.js
+++ listener.js
@@ -11,6 +11,17 @@ chrome.tabs.onUpdated.addListener(function(tabId, changeinfo, tab){
         jQuery.get('http://api.domaintools.com/v1/'+parser.hostname+'/', function(data){
             var registrarName = data.response.registration.registrar;
             console.log('Registrar', registrarName);
+
+            $.getJSON('registrarSiteDictionary.json', function(registrarListing) {
+                var registrarUrl = null;
+                for(var i=0; i<registrarListing.length; i++){
+                    if(registrarListing[i].name.toLowerCase() == registrarName.toLowerCase()){
+                        registrarUrl = registrarListing[i].url;
+                        console.log(registrarUrl);
+                    }
+                }
+                console.log('Done');
+            });
         });
     }
 });
